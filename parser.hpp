0ca5beb70711ea8e93567e53e20dd89fd7aac3b9
BigFish2086 <a8686.ibrahim@gmail.com>
2023-05-19 01:20:16 +0300

    changes in parser.hpp Changes are: - update the type of expression to return yytokentype - update all the Expr operations to have Expr* not just Expr, since   that is what being passed through yacc rules

--- parser.hpp
+++ parser.hpp
@@ -8,7 +8,9 @@
 #include <variant>
 #include <vector>
 
-#include "y.tab.h"
+#include "parser.tab.h"
+#include "quads.hpp"
+
 using namespace std;
 
 using Value = std::variant<int, float, std::string, bool>;
@@ -18,11 +20,19 @@ extern int yylineno;
 extern char *yytext;
 
 int current_scope = 0;
+int syntax_errors = 0;
 
 // Output files.
 string fout;
 ofstream symlog;
 ofstream output;
+
+// ----------------------------------------------------------------------
+#define log_syntax                                                             \
+  {                                                                            \
+    cerr << "STX(N#" << ++syntax_errors << "): Invalid syntax near '"          \
+         << yytext << "' in L#" << yylineno << endl;                           \
+  }
 
 // ----------------------------------------------------------------------
 // Global TODO:
@@ -162,12 +172,12 @@ struct log_not {
 
 // ----------------------------------------------------------------------
 struct type_op {
-  int operator()(const int &a) const { return INTEGER; }
-  int operator()(const float &a) const { return FLOAT; }
-  int operator()(const std::string &a) const { return STRING; }
-  int operator()(const bool &a) const { return BOOL; }
+  yytokentype operator()(const int &a) const { return INTEGER; }
+  yytokentype operator()(const float &a) const { return FLOAT; }
+  yytokentype operator()(const std::string &a) const { return STRING; }
+  yytokentype operator()(const bool &a) const { return BOOL; }
 
-  int operator()(auto a) const {
+  yytokentype operator()(auto a) const {
     throw std::runtime_error("not supported type");
   }
 };
@@ -183,61 +193,63 @@ struct Expr {
   Expr() : value(0), isConst(false) {}
   Expr(Value v) : value(v), isConst(false) {}
   Expr(Value v, bool ct) : value(v), isConst(ct) {}
-  bool constResult(const Expr &other) const { return isConst && other.isConst; }
+  bool constResult(const Expr *other) const {
+    return isConst && other->isConst;
+  }
 
   // overload type operator
-  int type() const { return std::visit(type_op(), value); }
+  yytokentype type() const { return std::visit(type_op(), value); }
 
   // overload assignment operator
-  Expr &operator=(const Expr &other) {
-    value = other.value;
-    isConst = other.isConst;
+  Expr &operator=(const Expr *other) {
+    value = other->value;
+    isConst = other->isConst;
     return *this;
   }
 
   // overload arithmatic operators
-  Expr operator-() const {
-    return Expr(std::visit(multplier(), value, Value(-1)), isConst);
+  Expr *operator-() const {
+    return new Expr(std::visit(multplier(), value, Value(-1)), isConst);
   }
-  Expr operator+(const Expr &other) const {
-    return Expr(std::visit(adder(), value, other.value),
-                this->constResult(other));
+  Expr *operator+(const Expr *other) const {
+    return new Expr(std::visit(adder(), value, other->value),
+                    this->constResult(other));
   }
-  Expr operator-(const Expr &other) const {
-    return Expr(std::visit(subtractor(), value, other.value),
-                this->constResult(other));
+  Expr *operator-(const Expr *other) const {
+    return new Expr(std::visit(subtractor(), value, other->value),
+                    this->constResult(other));
   }
-  Expr operator*(const Expr &other) const {
-    return Expr(std::visit(multplier(), value, other.value),
-                this->constResult(other));
+  Expr *operator*(const Expr *other) const {
+    return new Expr(std::visit(multplier(), value, other->value),
+                    this->constResult(other));
   }
-  Expr operator/(const Expr &other) const {
-    return Expr(std::visit(divider(), value, other.value),
-                this->constResult(other));
+  Expr *operator/(const Expr *other) const {
+    return new Expr(std::visit(divider(), value, other->value),
+                    this->constResult(other));
   }
 
   // overload comparison operators
-  bool operator==(const Expr &other) const {
-    return std::visit(equlizer(), value, other.value);
+  bool operator==(const Expr *other) const {
+    return std::visit(equlizer(), value, other->value);
   }
-  bool operator!=(const Expr &other) const { return !(*this == other); }
-  bool operator<(const Expr &other) const {
-    return std::visit(less_than(), value, other.value);
+  bool operator!=(const Expr *other) const { return !(*this == other); }
+  bool operator<(const Expr *other) const {
+    return std::visit(less_than(), value, other->value);
   }
-  bool operator>(const Expr &other) const { return other < *this; }
-  bool operator<=(const Expr &other) const { return !(*this > other); }
-  bool operator>=(const Expr &other) const { return !(*this < other); }
+  bool operator>(const Expr *other) const { return other < this; }
+  bool operator<=(const Expr *other) const { return !(*this > other); }
+  bool operator>=(const Expr *other) const { return !(*this < other); }
 
   // overload logical operators
   bool operator!() const { return std::visit(log_not(), value); }
-  bool operator&&(const Expr &other) const {
-    return std::visit(log_and(), value, other.value);
+  bool operator&&(const Expr *other) const {
+    return std::visit(log_and(), value, other->value);
   }
-  bool operator||(const Expr &other) const { return !(!(*this) && !other); }
+  bool operator||(const Expr *other) const { return !(!(*this) && !other); }
 
   // overload stream operator
-  friend ostream &operator<<(ostream &os, const Expr &expr) {
-    std::visit(printer(), expr.value);
+  friend ostream &operator<<(ostream &os, const Expr *expr) {
+    std::visit(printer(), expr->value);
     return os;
   }
 };
