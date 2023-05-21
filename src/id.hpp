1f57a833f9ce1213224dc7b89a6c7603d2e01382
BigFish2086 <a8686.ibrahim@gmail.com>
2023-05-21 21:43:03 +0300

    move ID Base class and their derived IDs i.e(VarID, FuncID, EnumID) to its own file Also update the VarID class to handle whether the variable is just a variable, const variable, enum variant or const enum variant

---
+++ src/id.hpp
@@ -0,0 +1,191 @@
+#pragma once
+
+#include "expr.hpp"
+#include "globals.hpp"
+
+class ID {
+public:
+  string name;
+  int line;
+  int scope;
+  bool isUsed;
+  yytokentype type;
+
+  ID(yytokentype _type, const string &_name) {
+    this->type = _type;
+    this->name = _name;
+    this->line = yylineno;
+    this->scope = current_scope;
+    this->isUsed = false;
+  }
+  virtual ~ID() {}
+
+  friend ostream &operator<<(ostream &os, const ID *id) {
+    os << "N: " << id->name << " L# " << id->line << " Scp@ " << id->scope
+       << " U: " << id->isUsed << " ";
+    return os;
+  }
+};
+
+// ----------------------------------------------------------------------
+class VarID : public ID {
+public:
+  Expr *expr;
+  bool isConst = false;
+  bool isInitialized = false;
+
+  bool isEnum = false;
+  string enumName = ""; // if the variable is an enumVariant
+
+  // for variables
+  VarID(yytokentype _type, const string &_name, bool isInit=false) : ID(_type, _name) {
+    this->isInitialized = isInit;
+  }
+
+  // for const variables
+  VarID(yytokentype _type, const string &_name, Expr *_expr)
+      : ID(_type, _name) {
+    this->__setExpr(_expr);
+    this->isConst = true;
+  }
+
+  // for enum variants
+  VarID(const string &_name, const string &_enumName)
+      : ID(ENUM_TYPE, _name) {
+    this->isEnum = true;
+    this->enumName = _enumName;
+  }
+
+  // for const enum variants
+  VarID(const string &_name, const string &_enumName, Expr *_expr)
+      : ID(ENUM_TYPE, _name) {
+    this->isEnum = true;
+    this->enumName = _enumName;
+    this->__setExpr(_expr);
+    this->isConst = true;
+  }
+
+  ~VarID() {
+    if (expr != nullptr) {
+      delete expr;
+    }
+  }
+  // TODO: call doCast() when needed
+  // checks are:
+  // 1. if the variable is const, then it cannot be assigned again
+  // 2. type of the variable and the expression must match or be convertible
+  void setExpr(Expr *_expr) {
+    if (this->isConst) {
+      error("cannot assign to const variable " + this->name);
+    }
+    this->__setExpr(_expr);
+  }
+
+  Expr *getExpr() {
+    if (!this->isInitialized) {
+      error("variable " + this->name + " is not initialized");
+    }
+    return this->expr;
+  }
+
+private:
+  void __setExpr(Expr *_expr) {
+    if (!canCast(this->type, _expr->type())) {
+      error("cannot assign Variable " + this->name + " of type " +
+            type2Str[this->type] + " to expression of type " +
+            type2Str[_expr->type()]);
+    }
+
+    if (this->isEnum) {
+      EnumExpr *enumExpr = dynamic_cast<EnumExpr *>(_expr);
+      if (enumExpr == nullptr) {
+        error("cannot assign non-enum expression to enum variable " +
+              this->name);
+      }
+      if (this->enumName != enumExpr->enumName) {
+        error("cannot assign enum expression of enum " + enumExpr->enumName +
+              " to enum variable " + this->name + " of enum " + this->enumName);
+      }
+    }
+
+    this->expr = _expr;
+    this->isInitialized = true;
+  }
+};
+
+// ----------------------------------------------------------------------
+struct StrList {
+  vector<string> list;
+  StrList() {}
+  StrList(string item) { this->append(item); }
+  StrList *append(string item) {
+    list.push_back(item);
+    return this;
+  }
+};
+
+struct TypedList {
+  vector<yytokentype> list;
+  string reprsentation = "";
+  TypedList() {}
+  TypedList(yytokentype item, const string &str) { this->append(item, str); }
+  TypedList *append(yytokentype item, const string &str) {
+    list.push_back(item);
+    reprsentation += str;
+    return this;
+  }
+  string repr() const { return reprsentation; }
+};
+
+class FuncID : public ID {
+public:
+  vector<yytokentype> funcParamsTypes;
+
+  FuncID(yytokentype _type, const string &_name,
+         vector<yytokentype> _paramsTypes)
+      : ID(_type, _name) {
+    this->funcParamsTypes = _paramsTypes;
+  }
+  FuncID(yytokentype _type, const string &_name, TypedList *_paramsTypes)
+      : ID(_type, _name) {
+    this->funcParamsTypes = _paramsTypes->list;
+  }
+  ~FuncID() {}
+};
+
+// ----------------------------------------------------------------------
+class EnumID : public ID {
+public:
+  vector<string> enumVariants; // TODO: change to set
+
+  EnumID(string _name, vector<string> _variants) : ID(ENUM_TYPE, _name) {
+    this->enumVariants = _variants;
+  }
+  EnumID(string _name, StrList *_variants) : ID(ENUM_TYPE, _name) {
+    this->enumVariants = _variants->list;
+  }
+  ~EnumID() {}
+
+  int getVariant(const string &variant) {
+    for (int i = 0; i < (int)this->enumVariants.size(); i++) {
+      if (this->enumVariants[i] == variant) {
+        return i;
+      }
+    }
+    error("enum " + this->name + " has no variant " + variant);
+    return -1;
+  }
+};
+
+// ----------------------------------------------------------------------
+struct IDStmt {
+  ID *id;
+  string reprsentation;
+  IDStmt(ID *_id) : id(_id) {}
+  IDStmt(ID *_id, const string &str) : id(_id), reprsentation(str) {}
+  ~IDStmt() { delete id; }
+
+  void setRepr(const string &str) { reprsentation = str; }
+  string repr() const { return reprsentation; }
+  ID *getID() const { return id; }
+};
