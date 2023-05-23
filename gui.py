0e4433bc7f9936fbbe8643cf5d3db80bf3b29f80
BigFish2086 <a8686.ibrahim@gmail.com>
2023-05-23 10:34:12 +0300

    second attempt is great i think, but still need to add events and bind them

--- gui.py
+++ gui.py
@@ -1,8 +1,10 @@
 from tkinter import *
+from tkinter import ttk
 import tkinter.scrolledtext as scrolledtext
 from tkinter.filedialog import askopenfilename
 import re
 
+# ====================================================================================================
 def search_re(pattern, text, groupid=0):
     matches = []
     text = text.splitlines()
@@ -26,7 +28,6 @@ function = rgb((95, 211, 234))
 background = rgb((42, 42, 42))
 font = 'Consolas 10'
 
-
 # Define a list of Regex Pattern that should be colored in a certain way
 repl = [
     ['(^| )(const|int|flt|bool|string|return|for|while|if|else|repeat|until|enum|true|false|switch|case|default)($| )', keywords],
@@ -36,16 +37,8 @@ repl = [
     ['print', function],
 ]
 
-# Setup Tkinter
-window_size = (1080, 720)
-frame_size = (int(window_size[0] * 0.90), int(window_size[1] - 20))
-root = Tk()
-root.title("A3E")
-root.configure(background=background)
-root.geometry(f"{window_size[0]}x{window_size[1]}")
-
+# ====================================================================================================
 codeText = ""
-
 # Event Handler for Coda Area
 def changes(event=None):
     global codeText
@@ -61,67 +54,112 @@ def changes(event=None):
             i+=1
     codeText = editArea.get('1.0', END) 
 
-    
-# Line Number Wrapper
-class LineNumbers(Text):
-    def __init__(self, master, text_widget, **kwargs):
-        super().__init__(master, **kwargs)
-        self.text_widget = text_widget
-        self.text_widget.bind('<KeyPress>', self.on_key_press)
-        self.insert(1.0, '1')
-        self.configure(state='disabled')
 
-    def on_key_press(self, event=None):
-        final_index = str(self.text_widget.index(END))
-        num_of_lines = final_index.split('.')[0]
-        line_numbers_string = "\n".join(str(no + 1) for no in range(int(num_of_lines)))
-        width = len(str(num_of_lines))
-        self.configure(state='normal', width=width)
-        self.delete(1.0, END)
-        self.insert(1.0, line_numbers_string)
-        self.configure(state='disabled')
+# ====================================================================================================
+# Setup Tkinter
+window_size = (1080, 720)
+frame_size = (int(window_size[0] * 0.90), int(window_size[1] - 20))
+root = Tk()
+root.title("A3E")
+root.configure(background=background)
+root.option_add("*tearOff", False)
 
+# Create a custom Style object with a background color
+style = ttk.Style()
+style.configure("Custom.TNotebook", background=background)
 
-# TOP FRAME
-topFrame = Frame(root, width=frame_size[0], height=frame_size[1], bg=background, borderwidth=5, relief="flat")
-symbolTable = scrolledtext.ScrolledText(topFrame, width=50, height=28, borderwidth=3, relief="ridge", wrap="none", font=font, bg=background, fg=normal)
-symbolTable.vbar.configure(bg=background)
-symbolTable.configure(state='disabled')
-editArea = Text(
-    topFrame,
+notebook = ttk.Notebook(root, style="Custom.TNotebook", padding=(10, 10, 10, 10))
+tab_1 = ttk.Frame(notebook)
+notebook.add(tab_1, text="   Editor   ")
+notebook.pack(fill="both", expand=True)
+editArea = scrolledtext.ScrolledText(
+    tab_1,
     bg=background,
     fg=normal,
     insertbackground=normal,
-    relief="ridge",
-    width=50, height=28,
+    relief="flat",
+    width=99, height=30,
     borderwidth=3,
-    font=font
+    font=font,
+    insertofftime=0,
+    padx=10,
+    pady=10,
 )
-lineNumbers = LineNumbers(topFrame, editArea, width=3, bg=background, fg=normal, borderwidth=0, font=font)
-
-# BOTTOM FRAME
-bottomFrame = Frame(root, width=frame_size[0], height=frame_size[1], bg=background, borderwidth=5, relief="flat")
-errorsArea = Text(bottomFrame, width=80, height=18, borderwidth=3, relief="ridge", wrap="none", font=font, bg=background, fg=normal)
-errorsArea.configure(state='disabled')
-compileBtn = Button(bottomFrame, text="Compile", width=15, height=1, fg='#2b2d35', font=font) # command=compile_code)
-selecFileBtn = Button(bottomFrame, text="Select file", width=15, height=1, fg='#2b2d35', font=font) # command=select_file)
-
-# PACKING TOP FRAME
-topFrame.pack()
-lineNumbers.pack(side="left", pady=30, fill="y")
-editArea.pack(side="left", fill="y", pady=30)
-symbolTable.pack(side="right", padx=20, pady=30)
-
-# PACKING BOTTOM FRAME
-bottomFrame.pack()
-errorsArea.pack(side="left", padx=(20, 20), pady=(0, 30))
-selecFileBtn.pack(side="top", pady=(0, 20))
-compileBtn.pack(side="top", pady=(0, 20))
-
-# BINDING EVENTS
-editArea.insert('1.0', "// Code Here")
+editArea.pack(fill=BOTH, expand=1)
+editArea.insert('1.0',
+"""
+// Code Here
+// <Ctrl+c> to compile
+// <Ctrl+r> to run
+// <Ctrl+i> to load a file
+""")
 editArea.bind('<KeyRelease>', changes)
 
-# MAIN LOOP
+# ====================================================================================================
+tab_2 = ttk.Frame(notebook)
+notebook.add(tab_2, text="   Quads   ")
+notebook.pack(fill="both", expand=True)
+quadArea = scrolledtext.ScrolledText(
+    tab_2,
+    bg=background,
+    fg=normal,
+    insertbackground=normal,
+    relief="flat",
+    width=99, height=30,
+    borderwidth=3,
+    font=font,
+    insertofftime=0,
+    padx=10,
+    pady=10,
+    state=DISABLED
+)
+quadArea.pack(fill=BOTH, expand=1)
+
+# ====================================================================================================
+tab_3 = ttk.Frame(notebook)
+notebook.add(tab_3, text="   Symbol Table   ")
+notebook.pack(fill="both", expand=True)
+symbolTable = scrolledtext.ScrolledText(
+    tab_3,
+    bg=background,
+    fg=normal,
+    insertbackground=normal,
+    relief="flat",
+    width=99, height=30,
+    borderwidth=3,
+    font=font,
+    insertofftime=0,
+    padx=10,
+    pady=10,
+    state=DISABLED
+)
+symbolTable.pack(fill=BOTH, expand=1)
+
+# ====================================================================================================
+tab_4 = ttk.Frame(notebook)
+notebook.add(tab_4, text="   Console   ")
+notebook.pack(fill="both", expand=True)
+console = scrolledtext.ScrolledText(
+    tab_4,
+    bg=background,
+    fg=normal,
+    insertbackground=normal,
+    relief="flat",
+    width=99, height=30,
+    borderwidth=3,
+    font=font,
+    insertofftime=0,
+    padx=10,
+    pady=10,
+    state=DISABLED
+)
+console.pack(fill=BOTH, expand=1)
+
+# ====================================================================================================
+
+
+
+
+
 changes()
 root.mainloop()
